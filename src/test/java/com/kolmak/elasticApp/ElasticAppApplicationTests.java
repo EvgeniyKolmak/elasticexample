90fedf385d9f14bced9f2056ae9a34548e98f2e2
Evgeniy Kolmak <zenakolmak669@gmail.com>
2024-10-20 18:39:59 +0300

    init web app

---
+++ src/test/java/com/kolmak/elasticApp/ElasticAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kolmak.elasticApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
